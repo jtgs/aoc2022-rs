958ae23839d73ac352bbdf8e2d96bb8831f89de2
James Stevens <jamesstevens@microsoft.com>
2022-12-02 09:49:15 +0000

    Day 2 solved

--- src/day02.rs
+++ src/day02.rs
@@ -1,7 +1,41 @@
+#[cfg(windows)]
+const LINE_ENDING: &'static str = "\r\n";
+#[cfg(not(windows))]
+const LINE_ENDING: &'static str = "\n";
+
 pub fn day02(input_lines: &str) -> (String, String) {
-    let _ = input_lines;
-    let answer1 = 0;
-    let answer2 = 0;
+    // A, X = Rock
+    // B, Y = Paper
+    // C, Z = Scissors
+    let answer1: i32 = input_lines.split(LINE_ENDING).map(|p| match p {
+        "A X" => 4, // 1 + 3 (Rock + Draw)
+        "A Y" => 8, // 2 + 6 (Paper + Win)
+        "A Z" => 3, // 3 + 0 (Scissors + Lose)
+        "B X" => 1, // 1 + 0
+        "B Y" => 5, // 2 + 3
+        "B Z" => 9, // 3 + 6
+        "C X" => 7, // 1 + 6
+        "C Y" => 2, // 2 + 0
+        "C Z" => 6, // 3 + 3
+        _ => panic!("Invalid combination!")
+    }).sum();
+
+    // X = Lose
+    // Y = Draw
+    // Z = Win
+    let answer2: i32 = input_lines.split(LINE_ENDING).map(|p| match p {
+        "A X" => 3, // 3 + 0
+        "A Y" => 4, // 1 + 3
+        "A Z" => 8, // 2 + 6
+        "B X" => 1, // 1 + 0
+        "B Y" => 5, // 2 + 3
+        "B Z" => 9, // 3 + 6
+        "C X" => 2, // 2 + 0
+        "C Y" => 6, // 3 + 3
+        "C Z" => 7, // 1 + 6
+        _ => panic!("Invalid combination!")
+    }).sum();
+
     (format!("{}", answer1), format!("{}", answer2))
 }
 
@@ -11,16 +45,16 @@ mod tests {
 
     #[test]
     fn check_day02_part1_case1() {
-        assert_eq!(day02("").0, "0".to_string())
+        assert_eq!(day02("A Y\r\nB X\r\nC Z").0, "15".to_string())
     }
 
     #[test]
     fn check_day02_part2_case1() {
-        assert_eq!(day02("").1, "0".to_string())
+        assert_eq!(day02("A Y\r\nB X\r\nC Z").1, "12".to_string())
     }
 
     #[test]
     fn check_day02_both_case1() {
-        assert_eq!(day02(""), ("0".to_string(), "0".to_string()))
+        assert_eq!(day02("A Y\r\nB X\r\nC Z"), ("15".to_string(), "12".to_string()))
     }
 }
