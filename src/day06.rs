1a3bcdec782ce43ae6766f6bdeb2c3a39c8deebc
James Stevens <jamesstevens@microsoft.com>
2022-12-06 11:39:05 +0000

    Day 6 solved

--- src/day06.rs
+++ src/day06.rs
@@ -1,7 +1,20 @@
-pub fn day06(input_lines: &str) -> (String, String) {
-    let _ = input_lines;
-    let answer1 = 0;
-    let answer2 = 0;
+use std::collections::HashSet;
+
+fn find_first_unique_set(input: &str, len: usize) -> usize {
+    for i in len-1..input.len() { // start at char #4
+        let start_ix = i - (len - 1);
+        let select_chars = input[start_ix..=i].to_owned();
+        let charset: HashSet<char> = select_chars.chars().collect();
+        if charset.len() == len {
+            return i + 1;
+        }
+    }
+    unreachable!()
+}
+
+pub fn day06(input_line: &str) -> (String, String) {
+    let answer1 = find_first_unique_set(input_line, 4);
+    let answer2 = find_first_unique_set(input_line, 14);
     (format!("{}", answer1), format!("{}", answer2))
 }
 
@@ -11,16 +24,24 @@ mod tests {
 
     #[test]
     fn check_day06_part1_case1() {
-        assert_eq!(day06("").0, "0".to_string())
+        assert_eq!(day06("mjqjpqmgbljsphdztnvjfqwrcgsmlb").0, "7".to_string());
+        assert_eq!(day06("bvwbjplbgvbhsrlpgdmjqwftvncz").0, "5".to_string());
+        assert_eq!(day06("nppdvjthqldpwncqszvftbrmjlhg").0, "6".to_string());
+        assert_eq!(day06("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg").0, "10".to_string());
+        assert_eq!(day06("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw").0, "11".to_string());
     }
 
     #[test]
     fn check_day06_part2_case1() {
-        assert_eq!(day06("").1, "0".to_string())
+        assert_eq!(day06("mjqjpqmgbljsphdztnvjfqwrcgsmlb").1, "19".to_string());
+        assert_eq!(day06("bvwbjplbgvbhsrlpgdmjqwftvncz").1, "23".to_string());
+        assert_eq!(day06("nppdvjthqldpwncqszvftbrmjlhg").1, "23".to_string());
+        assert_eq!(day06("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg").1, "29".to_string());
+        assert_eq!(day06("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw").1, "26".to_string());
     }
 
-    #[test]
-    fn check_day06_both_case1() {
-        assert_eq!(day06(""), ("0".to_string(), "0".to_string()))
-    }
+    // #[test]
+    // fn check_day06_both_case1() {
+    //     assert_eq!(day06(""), ("0".to_string(), "0".to_string()))
+    // }
 }
