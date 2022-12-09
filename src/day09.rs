09312547ac2c01aca15988d6238518613b9dddef
James Stevens <jamesstevens@microsoft.com>
2022-12-09 14:38:49 +0000

    Day 9 part 2 solved

--- src/day09.rs
+++ src/day09.rs
@@ -33,65 +33,88 @@ impl FromStr for Instruction {
     }
 }
 
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
+struct Point {
+    x: i32, 
+    y: i32,
+}
+
+fn move_rope(rope: &mut [Point], direction: &Direction) {
+    // Move the head
+    match direction {
+        Direction::Up => rope[0].y -= 1,
+        Direction::Down => rope[0].y += 1,
+        Direction::Left => rope[0].x -= 1,
+        Direction::Right => rope[0].x += 1,
+    }
+
+    // Move the remaining knots
+    for i in 1..rope.len() {
+        if rope[i-1].x - rope[i].x > 1 {
+            if rope[i-1].y < rope[i].y {
+                rope[i].y -= 1;
+            } else if rope[i-1].y > rope[i].y {
+                rope[i].y += 1;
+            }
+            rope[i].x += 1;
+        } else if rope[i-1].x - rope[i].x < -1 {
+            if rope[i-1].y < rope[i].y {
+                rope[i].y -= 1;
+            } else if rope[i-1].y > rope[i].y {
+                rope[i].y += 1;
+            }
+            rope[i].x -= 1;
+        } else if rope[i-1].y - rope[i].y > 1 {
+            if rope[i-1].x < rope[i].x {
+                rope[i].x -= 1;
+            } else if rope[i-1].x > rope[i].x {
+                rope[i].x += 1;
+            }
+            rope[i].y += 1;
+        } else if rope[i-1].y - rope[i].y < -1 {
+            if rope[i-1].x < rope[i].x {
+                rope[i].x -= 1;
+            } else if rope[i-1].x > rope[i].x {
+                rope[i].x += 1;
+            }
+            rope[i].y -= 1;
+        }
+    }
+}
+
 pub fn day09(input_lines: &str) -> (String, String) {
     let rows = split_into_lines(input_lines);
     let instructions: Vec<Instruction> = rows.iter().map(|s| Instruction::from_str(s).unwrap()).collect();
 
-    let mut cells_visited: HashSet<(i32, i32)> = HashSet::new();
+    let mut cells_visited_a: HashSet<Point> = HashSet::new();
+    let mut cells_visited_b: HashSet<Point> = HashSet::new();
 
-    let mut head_x = 0;
-    let mut head_y = 0;
-    let mut tail_x = 0;
-    let mut tail_y = 0;
-    cells_visited.insert((tail_x, tail_y));
+    let mut rope_a = [Point {x: 0, y:0}; 2];
+    cells_visited_a.insert(*rope_a.last().unwrap());
+
+    let mut rope_b = [Point {x: 0, y:0}; 10];
+    cells_visited_b.insert(*rope_b.last().unwrap());
 
     for instruction in instructions.iter() {
         for _ in 0..instruction.dist {
-            // Move the head
-            match &instruction.dir {
-                Direction::Up => head_y -= 1,
-                Direction::Down => head_y += 1,
-                Direction::Left => head_x -= 1,
-                Direction::Right => head_x += 1,
-            }
+            move_rope(&mut rope_a, &instruction.dir);
+            move_rope(&mut rope_b, &instruction.dir);
 
-            let old_tail = (tail_x, tail_y);
-            // Move the tail?
-            if head_x - tail_x > 1 {
-                if head_y != tail_y {
-                    tail_y = head_y;
-                }
-                tail_x += 1;
-            } else if head_x - tail_x < -1 {
-                if head_y != tail_y {
-                    tail_y = head_y;
-                }
-                tail_x -= 1;
-            } else if head_y - tail_y > 1 {
-                if head_x != tail_x {
-                    tail_x = head_x;
-                }
-                tail_y += 1;
-            } else if head_y - tail_y < -1 {
-                if head_x != tail_x {
-                    tail_x = head_x;
-                }
-                tail_y -= 1;
-            }
-
-            if (tail_x, tail_y) != old_tail {
-                cells_visited.insert((tail_x, tail_y));
-            }
+            // Record the position of the tail
+            cells_visited_a.insert(*rope_a.last().unwrap());
+            cells_visited_b.insert(*rope_b.last().unwrap());
         }
     }
 
-    let answer1 = cells_visited.len();
-    let answer2 = 0;
+    let answer1 = cells_visited_a.len();
+    let answer2 = cells_visited_b.len();
     (format!("{}", answer1), format!("{}", answer2))
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::helpers::load_input;
+
     use super::*;
 
     const TEST_INPUT: &str = "R 4
@@ -110,11 +133,27 @@ R 2";
 
     #[test]
     fn check_day09_part2_case1() {
-        assert_eq!(day09("").1, "0".to_string())
+        assert_eq!(day09(TEST_INPUT).1, "1".to_string());
+
+        let test_input_2 = "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20";
+        assert_eq!(day09(test_input_2).1, "36".to_string());
     }
 
     #[test]
     fn check_day09_both_case1() {
-        assert_eq!(day09(""), ("0".to_string(), "0".to_string()))
+        assert_eq!(day09(TEST_INPUT), ("13".to_string(), "1".to_string()))
+    }
+
+    #[test]
+    fn check_day09_puzzle() {
+        let input = load_input(9);
+        assert_eq!(day09(&input), ("5930".to_string(), "2443".to_string()))
     }
 }
